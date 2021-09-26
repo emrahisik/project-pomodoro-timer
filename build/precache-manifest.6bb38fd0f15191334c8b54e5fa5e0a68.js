4637fb79ca7703189a36afee67764817f10e8a70
Emrah Isik <emrahisk@gmail.com>
2021-09-26 17:36:22 -0400

    deployment

---
+++ build/precache-manifest.6bb38fd0f15191334c8b54e5fa5e0a68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4354e27648f311a115d7aa072bbc33fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcf2358e13059ea0178a",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcf2358e13059ea0178a",
+    "url": "/static/js/main.d84b40c6.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
